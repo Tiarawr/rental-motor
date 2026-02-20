de9ef96cbbf7d925977ff44623052854184e811d
tiarawr <nailaym@students.unnes.ac.id>
2026-02-21 02:26:49 +0700

    feat: add configured Axios instance with token interceptor

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "https://api.jogjarentalan.my.id/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
